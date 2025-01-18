ded6dd045248d072ea5efa1a3fb735177af4b389
Vadzim_Hardzenia <vadimgordenya@gmail.com>
2025-01-18 12:10:30 +0200

    feat: Add antd package. Configure base AntdThemeProvider.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import {Button} from 'antd';
+
 export default function Home() {
   return (
-    <h1>NextJS - SocketIO Chat</h1>
+    <>
+      <h1>NextJS - SocketIO Chat</h1>
+      <Button type="primary">Test Button</Button>
+    </>
   )
 }
---
+++ src/providers/theme-provider.tsx
@@ -0,0 +1,28 @@
+'use client';
+import React from 'react';
+import { ConfigProvider } from 'antd'
+
+function ThemeProvider({ children }: { children: React.ReactNode }) {
+  return <div>
+    <ConfigProvider theme={{
+      token: {
+        colorPrimary: '#31304D',
+        borderRadius: 2,
+      },
+      components: {
+        Button: {
+          controlHeight: 35,
+          boxShadow: 'none',
+          colorPrimaryBgHover: '#31304D',
+          colorPrimaryHover: '#31304D',
+          controlOutline: 'none',
+          colorBorder: '#31304D'
+        }
+      }
+    }}>
+      {children}
+    </ConfigProvider>
+  </div>
+}
+
+export default ThemeProvider;
